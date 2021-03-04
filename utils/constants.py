9a07784b70d4bc94ee2547aa17e56077675c7b96
Amenly <60410674+Amenly@users.noreply.github.com>
2021-03-04 11:12:48 -0600

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,5 @@
 MODEL_URL = 'https://mym.fans/{}'
-POSTS_URL = 'https://mym.fans/includes/infinite-scroll/profile-datas.php?action=get&idm_men_connecte={}&id_men_connecte={}&idg={}&last_msg_id={}&last_msg_date_add={}'
+POSTS_URL = 'https://mym.fans/includes/infinite-scroll/profile-datas.php?action=get&idm_men_connecte={}&id_men_connecte={}&idg={}&token_infinite=tau0SyR3-hiZd0-sc8LzjpFkxKjXKZV383tQOn4RmBn_00e7BM&last_msg_id={}&last_msg_date_add={}'
 SUBSCRIPTION_URL = 'https://mym.fans/subscriptions.php'
 FAVORITES_URL = 'https://mym.fans/subscriptions.php?show=favorites'
 
